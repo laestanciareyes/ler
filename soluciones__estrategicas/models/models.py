d2f7ec1ab439f1450880b17f145db0c75dfb3f4f
erickjairsm <odoo@ampd2.odoo.com>
2020-12-04 00:37:40 +0000

    Modulo La Estancia Reyes

--- soluciones__estrategicas/models/models.py
+++ soluciones__estrategicas/models/models.py
@@ -257,25 +257,68 @@ class AgregarCamposProductos(models.Model):
 
 #################################
 #Campos Facturas (Documentos)
-#################################  
+################################# 
+#@api.depends('order.id','order.partner_id')
 class AgregarCamposFactura(models.TransientModel):
     
     def _get_solicitudes(self):
         domain =[('id', '=', -1)]
         solicitudes_list=[]
         _logger.info('_get_solicitudes domain 3.0 = ')
-        _logger.info(self.solicitud_id)
-        solicitudes_model = self.env['solicitudes.credito.lineas'].search([('cliente_id.id','=',self.solicitud_id.cliente_id.id)])
+        solicitudes_model = self.env['solicitudes.credito.lineas'].search([('cliente_id.id','=',-2),('estatus','=','A')])
+        #_logger.info(self.order.partner_id)
+        #if self._context.get('active_model') == 'sale.order' and self._context.get('active_id', False):
+        if self._context.get('active_model') == 'sale.order' and self._context.get('active_id', False):
+            sale_order = self.env['sale.order'].browse(self._context.get('active_id'))
+            _logger.info('************************************************')
+            _logger.info(sale_order.partner_id.id)
+            _logger.info('************************************************')
+            domain
+            solicitudes_model = self.env['solicitudes.credito.lineas'].search([('cliente_id.id','=',sale_order.partner_id.id),('estatus','=','A')])
         #,('estatus','=','A')
         for each in solicitudes_model:
-            solicitudes_list.append(each.solicitud_id.id)
+            solicitudes_list.append(each.id)
         if solicitudes_list:
             domain =[('id', 'in', solicitudes_list)]
-            _logger.info('_get_solicitudes domain = ' + domain[0] + domain[1])
-            return domain
-        _logger.info('_get_solicitudes domain = ')
-        _logger.info(domain)
+            #_logger.info('_get_solicitudes domain = ' + domain[0] + domain[1])
+        #    return domain
+        _logger.info('_get_solicitudes domain CONTEO = ')
+        _logger.info(len(domain))
         return domain
+    
+    def _prepare_invoice_values(self, order, name, amount, so_line):
+        invoice_vals = {
+            'ref': order.client_order_ref,
+            'type': 'out_invoice',
+            'invoice_origin': order.name,
+            'invoice_user_id': order.user_id.id,
+            'narration': order.note,
+            'partner_id': order.partner_invoice_id.id,
+            'fiscal_position_id': order.fiscal_position_id.id or order.partner_id.property_account_position_id.id,
+            'partner_shipping_id': order.partner_shipping_id.id,
+            'currency_id': order.pricelist_id.currency_id.id,
+            'invoice_payment_ref': order.reference,
+            'invoice_payment_term_id': order.payment_term_id.id,
+            'invoice_partner_bank_id': order.company_id.partner_id.bank_ids[:1].id,
+            'team_id': order.team_id.id,
+            'campaign_id': order.campaign_id.id,
+            'medium_id': order.medium_id.id,
+            'source_id': order.source_id.id,
+            'invoice_line_ids': [(0, 0, {
+                'name': name,
+                'price_unit': amount,
+                'quantity': 1.0,
+                'product_id': self.product_id.id,
+                'product_uom_id': so_line.product_uom.id,
+                'tax_ids': [(6, 0, so_line.tax_id.ids)],
+                'sale_line_ids': [(6, 0, [so_line.id])],
+                'analytic_tag_ids': [(6, 0, so_line.analytic_tag_ids.ids)],
+                'analytic_account_id': order.analytic_account_id.id or False,
+            })],
+        }
+        _logger.info('########### Preparando información de orden hacia factura...')
+        _logger.info(self.order.partner_id)
+        return invoice_vals
     
     
     _inherit = 'sale.advance.payment.inv'
