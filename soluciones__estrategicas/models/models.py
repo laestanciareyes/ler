3f376e5d20ec84b2a6f773be5ea698afdb8d472c
erickjairsm <odoo@ampd2.odoo.com>
2020-12-24 00:51:48 +0000

    Modulo La Estancia Reyes

--- soluciones__estrategicas/models/models.py
+++ soluciones__estrategicas/models/models.py
@@ -850,12 +850,116 @@ class AgregarCamposFactura(models.Model):
 #Campos Pago
 #################################       
     
-class AgregarCamposPagos(models.Model):
+class AccountPayment(models.Model):
     _inherit = 'account.payment'
     _description = 'Campos Adicionales para pagos '
     
     solicitud_id = fields.Many2one('solicitudes.credito.lineas','Solicitud de Crédito', help='Seleccione una solicitud del cliente o deje en blanco en caso que sea compra sin crédito')
     cuota_id = fields.Many2one('solicitudes.credito.lineas.cuotas','Cuota', help='Seleccione una cuota del cliente o deje en blanco en caso que sea compra sin crédito')
+    
+    
+    def _get_payment_chatter_link(self):
+        self.ensure_one()
+        return '<a href=# data-oe-model=account.payment data-oe-id=%d>%s</a>' % (self.id, self.name)
+
+    @api.onchange('partner_id', 'payment_method_id', 'journal_id')
+    def _onchange_set_payment_token_id(self):
+        res = {}
+
+        if not self.payment_method_code == 'electronic' or not self.partner_id or not self.journal_id:
+            self.payment_token_id = False
+            return res
+
+        partners = self.partner_id | self.partner_id.commercial_partner_id | self.partner_id.commercial_partner_id.child_ids
+        domain = [('partner_id', 'in', partners.ids), ('acquirer_id.journal_id', '=', self.journal_id.id)]
+        self.payment_token_id = self.env['payment.token'].search(domain, limit=1)
+
+        res['domain'] = {'payment_token_id': domain}
+        return res
+
+    def _prepare_payment_transaction_vals(self):
+        self.ensure_one()
+        return {
+            'amount': self.amount,
+            'currency_id': self.currency_id.id,
+            'partner_id': self.partner_id.id,
+            'partner_country_id': self.partner_id.country_id.id,
+            'invoice_ids': [(6, 0, self.invoice_ids.ids)],
+            'payment_token_id': self.payment_token_id.id,
+            'acquirer_id': self.payment_token_id.acquirer_id.id,
+            'payment_id': self.id,
+            'type': 'server2server',
+        }
+
+    def _create_payment_transaction(self, vals=None):
+        for pay in self:
+            if pay.payment_transaction_id:
+                raise ValidationError(_('A payment transaction already exists.'))
+            elif not pay.payment_token_id:
+                raise ValidationError(_('A token is required to create a new payment transaction.'))
+
+        transactions = self.env['payment.transaction']
+        for pay in self:
+            transaction_vals = pay._prepare_payment_transaction_vals()
+
+            if vals:
+                transaction_vals.update(vals)
+
+            transaction = self.env['payment.transaction'].create(transaction_vals)
+            transactions += transaction
+
+            # Link the transaction to the payment.
+            pay.payment_transaction_id = transaction
+            
+        cuota_id=-2
+        cuota_id = self.cuota_id
+            
+        cuotafechapagada = self.payment_date
+        cuotamontorecibido = self.amount
+        qry=""
+            
+        if cuota_id:
+            qry="update solicitudes_credito_lineas_cuotas set cuotaestatus='P', cuotafechapagada='" + str(cuotafechapagada) + "' + cuotamontorecibido=" + str(cuotamontorecibido)+" where id=" + str(cuota_id)
+            
+        _logger.info("******** QRY *********")
+        _logger.info("CUOTA: " + str(cuota_id))
+        _logger.info(qry)
+
+        return transactions
+
+    def action_validate_invoice_payment(self):
+        res = super(AccountPayment, self).action_validate_invoice_payment()
+        self.mapped('payment_transaction_id').filtered(lambda x: x.state == 'done' and not x.is_processed)._post_process_after_done()
+        return res
+
+    def post(self):
+        # Post the payments "normally" if no transactions are needed.
+        # If not, let the acquirer updates the state.
+        #                                __________            ______________
+        #                               | Payments |          | Transactions |
+        #                               |__________|          |______________|
+        #                                  ||                      |    |
+        #                                  ||                      |    |
+        #                                  ||                      |    |
+        #  __________  no s2s required   __\/______   s2s required |    | s2s_do_transaction()
+        # |  Posted  |<-----------------|  post()  |----------------    |
+        # |__________|                  |__________|<-----              |
+        #                                                |              |
+        #                                               OR---------------
+        #  __________                    __________      |
+        # | Cancelled|<-----------------| cancel() |<-----
+        # |__________|                  |__________|
+
+        payments_need_trans = self.filtered(lambda pay: pay.payment_token_id and not pay.payment_transaction_id)
+        transactions = payments_need_trans._create_payment_transaction()
+
+        res = super(AccountPayment, self - payments_need_trans).post()
+
+        transactions.s2s_do_transaction()
+
+        return res
+    
+    
     
     def name_get(self):
         
@@ -869,7 +973,7 @@ class AgregarCamposPagos(models.Model):
         else:
             partner = self.partner_id.id
             
-        qry = "SELECT a.id, a.name, a.cuotanumero from solicitudes_credito_lineas_cuotas a inner join solicitudes_credito_lineas b on b.id = a.solicitud_id where b.cliente_id=" + str(partner) + " and b.cuotaestatus='E' order by cuotanumero"
+        qry = "SELECT a.id, a.name, a.cuotanumero from solicitudes_credito_lineas_cuotas a inner join solicitudes_credito_lineas b on b.id = a.solicitud_id where b.cliente_id=" + str(partner) + " and a.cuotaestatus='E' order by cuotanumero"
         
         self.env.cr.execute(qry)
         #self.deadline = env.cr.fetchone()[0]
